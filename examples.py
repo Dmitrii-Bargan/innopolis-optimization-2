51a35559fa61dac32dac13f7b9bb89d3e30eb02c
Dmitrii-Bargan <d.bargan@innopolis.university>
2024-11-02 20:40:24 +0300

    Refactoring

--- examples.py
+++ examples.py
@@ -4,179 +4,131 @@ from interior_point_solver import InteriorPointSolver
 
 def main() -> None:
     print("Example 1")
-    print()
-    print("Intetior point alpha = 0,5: ")
-    solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MAXIMIZE,
-                                 c=[9, 10, 16, 0, 0, 0],
-                                 a=[
-                                     [18, 15, 12,1,0,0],
-                                     [6, 4, 8,0,1,0],
-                                     [5, 3, 3,0,0,1]
-                                 ],
-                                 b=[360, 192, 180],
-                                 start=[1, 1, 1, 315, 174, 169],
-                                 alpha=0.5,
-                                 eps=5)
+    print("Interior point alpha = 0,5: ")
+    InteriorPointSolver(
+        mode=InteriorPointSolver.Mode.MAXIMIZE,
+        c=[9, 10, 16, 0, 0, 0],
+        a=[
+            [18, 15, 12, 1, 0, 0],
+            [6, 4, 8, 0, 1, 0],
+            [5, 3, 3, 0, 0, 1]
+        ],
+        b=[360, 192, 180],
+        start=[1, 1, 1, 315, 174, 169],
+        alpha=0.5,
+        eps=5
+    ).solve()
 
-    solver.solve()
-
-    print()
     print("Interior point alpha = 0,9: ")
-    solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MAXIMIZE,
-                                 c=[9, 10, 16, 0, 0, 0],
-                                 a=[
-                                     [18, 15, 12,1,0,0],
-                                     [6, 4, 8,0,1,0],
-                                     [5, 3, 3,0,0,1]
-                                 ],
-                                 b=[360, 192, 180],
-                                 start=[1, 1, 1, 315, 174, 169],
-                                 alpha=0.9,
-                                 eps=5)
-
-    solver.solve()
+    InteriorPointSolver(
+        mode=InteriorPointSolver.Mode.MAXIMIZE,
+        c=[9, 10, 16, 0, 0, 0],
+        a=[
+            [18, 15, 12, 1, 0, 0],
+            [6, 4, 8, 0, 1, 0],
+            [5, 3, 3, 0, 0, 1]
+        ],
+        b=[360, 192, 180],
+        start=[1, 1, 1, 315, 174, 169],
+        alpha=0.9,
+        eps=5
+    ).solve()
 
     print()
     print("Simplex: ")
-    solver = SimplexSolver(mode=SimplexSolver.Mode.MAXIMIZE,
-                           c=[9, 10, 16],
-                           a=[
-                               [18, 15, 12],
-                               [6, 4, 8],
-                               [5, 3, 3]
-                           ],
-                           b=[360, 192, 180],
-                           eps=5)
-
-    solver.solve()
-
+    SimplexSolver(
+        mode=SimplexSolver.Mode.MAXIMIZE,
+        c=[9, 10, 16],
+        a=[
+            [18, 15, 12],
+            [6, 4, 8],
+            [5, 3, 3]
+        ],
+        b=[360, 192, 180],
+        eps=5
+    ).solve()
 
     print()
     print("--> Example 2. An unbounded objective function")
 
-    print("Intetior point alpha = 0,5: ")
-    solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MAXIMIZE,
-                                 c=[5, 4, 0, 0],
-                                 a=[
-                                     [1, 0,1,0],
-                                     [1, -1,0,1]
-                                 ],
-                                 b=[7, 8],
-                                 start=[1,1,6,8],
-                                 alpha=0.5,
-                                 eps=5)
+    print("Interior point alpha = 0,5: ")
+    InteriorPointSolver(
+        mode=InteriorPointSolver.Mode.MAXIMIZE,
+        c=[5, 4, 0, 0],
+        a=[
+            [1, 0, 1, 0],
+            [1, -1, 0, 1]
+        ],
+        b=[7, 8],
+        start=[1, 1, 6, 8],
+        alpha=0.5,
+        eps=5
+    ).solve()
 
-    solver.solve()
-
-    print()
-    print("Intetior point alpha = 0,9: ")
-    solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MAXIMIZE,
-                                 c=[5, 4, 0, 0],
-                                 a=[
-                                     [1, 0,1,0],
-                                     [1, -1,0,1]
-                                 ],
-                                 b=[7, 8],
-                                 start=[1,1,6,8],
-                                 alpha=0.9,
-                                 eps=5)
-
-    solver.solve()
+    print("Interior point alpha = 0,9: ")
+    InteriorPointSolver(
+        mode=InteriorPointSolver.Mode.MAXIMIZE,
+        c=[5, 4, 0, 0],
+        a=[
+            [1, 0, 1, 0],
+            [1, -1, 0, 1]
+        ],
+        b=[7, 8],
+        start=[1, 1, 6, 8],
+        alpha=0.9,
+        eps=5
+    ).solve()
 
     print()
     print("Simplex: ")
-    solver = SimplexSolver(mode=SimplexSolver.Mode.MAXIMIZE,
-                           c=[5, 4],
-                           a=[
-                               [1, 0],
-                               [1, -1]
-                           ],
-                           b=[7, 8],
-                           eps=5,)
+    SimplexSolver(
+        mode=SimplexSolver.Mode.MAXIMIZE,
+        c=[5, 4],
+        a=[
+            [1, 0],
+            [1, -1]
+        ],
+        b=[7, 8],
+        eps=5
+    ).solve()
 
-    solver.solve()
-
-    # print()
-    # print("--> Example 3. Another unbounded function")
-    # simplex_solve_and_check(
-    #     mode=SimplexSolver.Mode.MAXIMIZE,
-    #     objective_function=[5, 4],
-    #     constraints_matrix=[
-    #         [1, -1],
-    #         [1, 0]
-    #     ],
-    #     constraints_right_hand_side=[8, 7],
-    #     epsilon=5,
-    #     expected_solution=None
-    # )
-    #
-    # print()
-    # print("--> Example 4")
-    # simplex_solve_and_check(
-    #     mode=SimplexSolver.Mode.MAXIMIZE,
-    #     objective_function=[1, 2, 3],
-    #     constraints_matrix=[
-    #         [1, 1, 1],
-    #         [2, 1, 1],
-    #     ],
-    #     constraints_right_hand_side=[10, 20],
-    #     epsilon=5,
-    #     expected_solution=30
-    # )
-    #
-    # print()
-    # print("--> Example 5 - An unbounded function. Taken from lab 5.1, task 2")
-    # simplex_solve_and_check(
-    #     mode=SimplexSolver.Mode.MAXIMIZE,
-    #     objective_function=[2, 3],
-    #     constraints_matrix=[
-    #         [4, -2],
-    #         [-1, -1]
-    #     ],
-    #     constraints_right_hand_side=[-4, -6],
-    #     epsilon=5,
-    #     expected_solution=None
-    # )
-    #
-    # print()
     print("--> Example 6 - More variables. Taken from lab 2, task 1")
 
-    print("Intetior point alpha = 0,5: ")
-    solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MAXIMIZE,
-                                 c=[100, 140, 120, 0, 0, 0,0],
-                                 a=[
-                                     [3, 6, 7, 1, 0, 0,0],
-                                     [2, 1, 8, 0, 1, 0,0],
-                                     [1, 1, 1, 0, 0, 1,0],
-                                     [5, 3, 3, 0, 0, 0,1]
-                                 ],
-                                 b=[135, 260, 220, 360],
-                                 start=[1, 1, 1, 119, 249, 117,349],
-                                 alpha=0.5,
-                                 eps=5)
+    print("Interior point alpha = 0,5: ")
+    InteriorPointSolver(
+        mode=InteriorPointSolver.Mode.MAXIMIZE,
+        c=[100, 140, 120, 0, 0, 0, 0],
+        a=[
+            [3, 6, 7, 1, 0, 0, 0],
+            [2, 1, 8, 0, 1, 0, 0],
+            [1, 1, 1, 0, 0, 1, 0],
+            [5, 3, 3, 0, 0, 0, 1]
+        ],
+        b=[135, 260, 220, 360],
+        start=[1, 1, 1, 119, 249, 117, 349],
+        alpha=0.5,
+        eps=5
 
-    solver.solve()
-
+    ).solve()
 
     print("Intetior point alpha = 0,9: ")
-    solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MAXIMIZE,
-                                 c=[100, 140, 120, 0, 0, 0,0],
-                                 a=[
-                                     [3, 6, 7, 1, 0, 0,0],
-                                     [2, 1, 8, 0, 1, 0,0],
-                                     [1, 1, 1, 0, 0, 1,0],
-                                     [5, 3, 3, 0, 0, 0,1]
-                                 ],
-                                 b=[135, 260, 220, 360],
-                                 start=[1, 1, 1, 119, 249, 117,349],
-                                 alpha=0.9,
-                                 eps=5)
-
-    solver.solve()
-
+    InteriorPointSolver(
+        mode=InteriorPointSolver.Mode.MAXIMIZE,
+        c=[100, 140, 120, 0, 0, 0, 0],
+        a=[
+            [3, 6, 7, 1, 0, 0, 0],
+            [2, 1, 8, 0, 1, 0, 0],
+            [1, 1, 1, 0, 0, 1, 0],
+            [5, 3, 3, 0, 0, 0, 1]
+        ],
+        b=[135, 260, 220, 360],
+        start=[1, 1, 1, 119, 249, 117, 349],
+        alpha=0.9,
+        eps=5
+    ).solve()
 
     print("Simplex: ")
-    solver = SimplexSolver(
+    SimplexSolver(
         mode=SimplexSolver.Mode.MAXIMIZE,
         c=[100, 140, 120],
         a=[
@@ -187,56 +139,51 @@ def main() -> None:
         ],
         b=[135, 260, 220, 360],
         eps=5,
-    )
+    ).solve()
 
-    solver.solve()
-
-    print()
     print("--> Example 7 - Minimization, taken from tutorial")
-    print("Intetior point alpha = 0,5: ")
-    solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MINIMIZE,
-                                 c=[-2, 2, -6, 0, 0, 0],
-                                 a=[
-                                     [2, 1, -2, 1, 0, 0],
-                                     [1, 2, 4, 0, 1, 0],
-                                     [1, -1, 2, 0, 0, 1]
-                                 ],
-                                 b=[24, 23, 10],
-                                 start=[1, 1, 1, 23, 16, 8],
-                                 alpha=0.5,
-                                 eps=5)
-
-    solver.solve()
-
+    print("Interior point alpha = 0,5: ")
+    solver = InteriorPointSolver(
+        mode=InteriorPointSolver.Mode.MINIMIZE,
+        c=[-2, 2, -6, 0, 0, 0],
+        a=[
+            [2, 1, -2, 1, 0, 0],
+            [1, 2, 4, 0, 1, 0],
+            [1, -1, 2, 0, 0, 1]
+        ],
+        b=[24, 23, 10],
+        start=[1, 1, 1, 23, 16, 8],
+        alpha=0.5,
+        eps=5
+    ).solve()
 
     print("Intetior point alpha = 0,9: ")
-    solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MINIMIZE,
-                                 c=[-2, 2, -6, 0, 0, 0],
-                                 a=[
-                                     [2, 1, -2, 1, 0, 0],
-                                     [1, 2, 4, 0, 1, 0],
-                                     [1, -1, 2, 0, 0, 1]
-                                 ],
-                                 b = [24, 23, 10],
-                                 start=[1, 1, 1, 23, 16, 8],
-                                 alpha=0.9,
-                                 eps=5)
-
-    solver.solve()
-
+    InteriorPointSolver(
+        mode=InteriorPointSolver.Mode.MINIMIZE,
+        c=[-2, 2, -6, 0, 0, 0],
+        a=[
+            [2, 1, -2, 1, 0, 0],
+            [1, 2, 4, 0, 1, 0],
+            [1, -1, 2, 0, 0, 1]
+        ],
+        b=[24, 23, 10],
+        start=[1, 1, 1, 23, 16, 8],
+        alpha=0.9,
+        eps=5
+    ).solve()
 
     print("Simplex: ")
-    solver = SimplexSolver(mode=InteriorPointSolver.Mode.MINIMIZE,
-                           c=[-2, 2, -6],
-                           a=[
-                               [2, 1, -2],
-                               [1, 2, 4],
-                               [1, -1, 2]
-                           ],
-                           b=[24, 23, 10],
-                           eps=5)
-
-    solver.solve()
+    SimplexSolver(
+        mode=InteriorPointSolver.Mode.MINIMIZE,
+        c=[-2, 2, -6],
+        a=[
+            [2, 1, -2],
+            [1, 2, 4],
+            [1, -1, 2]
+        ],
+        b=[24, 23, 10],
+        eps=5
+    ).solve()
 
 
 if __name__ == "__main__":
--- interior_point_solver.py
+++ interior_point_solver.py
@@ -13,7 +13,16 @@ class InteriorPointSolver:
         MAXIMIZE = "Maximize"
         MINIMIZE = "Minimize"
 
-    def __init__(self, mode: Mode, start: list[float], c: list[float], a: list[list[float]], b: list[float], alpha: float, eps: int) -> None:
+    def __init__(
+            self,
+            mode: Mode,
+            start: list[float],
+            c: list[float],
+            a: list[list[float]],
+            b: list[float],
+            alpha: float,
+            eps: int
+    ) -> None:
         """
         Construct Interior-Point Algorithm problem solver
 
@@ -38,7 +47,6 @@ class InteriorPointSolver:
 
         self.b: list[float] = b
         """Right hand side of the constraints equations"""
-
 
         self.start: list[float] = start
         """Initial point"""
@@ -66,6 +74,7 @@ class InteriorPointSolver:
         self.c = np.transpose(self.c)
         exception = None
         i = 1
+
         while True:
             v = x
             d = np.diag(x)
@@ -73,38 +82,47 @@ class InteriorPointSolver:
             cc = np.dot(d, self.c)
             I = np.eye(len(self.c))
             f = np.dot(aa, np.transpose(aa))
+
             if np.any(np.isnan(f)) or np.any(np.isinf(f)):
                 x = None
                 exception = "The problem does not have solution!"
                 break
+
             if np.linalg.det(f) == 0:
                 x = None
                 self.is_not_applicable = True
                 exception = "The method is not applicable!"
                 break
+
             f_inverse = np.linalg.inv(f)
             H = np.dot(np.transpose(aa), f_inverse)
             P = np.subtract(I, np.dot(H, aa))
             cp = np.dot(P, cc)
+
             if np.any(np.isnan(cp)) or np.any(np.isinf(cp)):
                 x = None
                 exception = "The problem does not have solution!"
                 break
+
             if np.all(cp >= 0):
                 x = None
                 exception = "The problem does not have solution!"
                 break  # Решение неограниченно, выход из функции
+
             nu = np.absolute(np.min(cp))
             if nu < 1e-10:
                 x = None
                 exception = "The problem does not have solution!"
                 break
-            y = np.add(np.ones(len(self.c), float), (self.alpha/nu) * cp)
+
+            y = np.add(np.ones(len(self.c), float), (self.alpha / nu) * cp)
             yy = np.dot(d, y)
             x = yy
-            if norm(np.subtract(yy, v), ord = 2) <  0.00001:
+
+            if norm(np.subtract(yy, v), ord=2) < 0.00001:
                 break
-            i+= 1
+
+            i += 1
         return x
 
     def solve(self) -> Union[tuple[float, list[float]], None]:
@@ -113,20 +131,18 @@ class InteriorPointSolver:
         or "Unbounded" if the objective function is unbounded
         :return: a tuple (solution, X*) or [None] if the objective function is unbounded
         """
-        x =self.calculate()
-
-
-
+        x = self.calculate()
 
         if not self.is_not_applicable and x is not None:
-            x = [round(float(i),self.eps) for i in x]
+            x = [round(float(i), self.eps) for i in x]
             for i in range(len(x)):
-                self.solution += self.c[i]*x[i]
+                self.solution += self.c[i] * x[i]
 
             if self.mode == InteriorPointSolver.Mode.MINIMIZE:  # Flip the solution in case we were minimizing
                 self.solution *= -1
+
             print("X: ", x)
-            print("Solution:",self.solution)
+            print("Solution:", self.solution)
             return self.solution, x
         elif self.is_not_applicable:
             print("The method is not applicable!")
@@ -156,8 +172,9 @@ def interior_point_solve_and_check(
     :param epsilon: optimization accuracy. Number of digits after the floating point to consider
     :param expected_solution: the expected solution of the optimization problem
     """
-    solver = InteriorPointSolver(mode,start, objective_function, constraints_matrix, constraints_right_hand_side,alpha, epsilon)
-    solutions,x = solver.solve()
+    solver = InteriorPointSolver(mode, start, objective_function, constraints_matrix, constraints_right_hand_side,
+                                 alpha, epsilon)
+    solutions, x = solver.solve()
 
     if solutions is None:  # Unbounded function
         if expected_solution is not None:
@@ -170,13 +187,14 @@ def interior_point_solve_and_check(
         for i in range(len(objective_function)):
             answer += objective_function[i] * solutions[1][i]
 
-        if not isclose(answer, expected_solution, rel_tol=10**(-epsilon)):
+        if not isclose(answer, expected_solution, rel_tol=10 ** (-epsilon)):
             raise ArithmeticError("X* coefficients do not produce expected solution")
 
         for i in range(len(constraints_right_hand_side)):
             calculated = sum([constraints_matrix[i][j] * solutions[1][j] for j in range(len(solutions[1]))])
             if calculated > constraints_right_hand_side[i]:
                 raise ArithmeticError(f"Produced X* does not match {i}-th constraint")
+
 
 def read_data():
     _, ln_start, _, ln_C, _, *lns_A, _, ln_b = (ln.strip() for ln in stdin.readlines())
@@ -185,56 +203,3 @@ def read_data():
     A = [[float(x) for x in ln.strip().split()] for ln in lns_A]
     b = [float(x) for x in ln_b.strip().split()]
     return C, A, b, start
-
-
-print("Example 1")
-print("Intetior point alpha = 0,5")
-solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MINIMIZE,
-                             c=[-2, 2, -6, 0, 0, 0],
-                             a=[
-                                 [2, 1, -2, 1, 0, 0],
-                                 [1, 2, 4, 0, 1, 0],
-                                 [1, -1, 2, 0, 0, 1]
-                             ],
-                             b=[24, 23, 10],
-                             start=[1, 1, 1, 23, 16, 8],
-                             alpha=0.5,
-                             eps=5)
-
-solver.solve()
-# if sol is not None:
-#     print("ans for alpha = 0.5")
-#     print("X: ", x1)
-#     print(sol)
-# else:
-#     print("ans for alpha = 0.5")
-
-
-print("Intetior point alpha = 0,9")
-solver = InteriorPointSolver(mode=InteriorPointSolver.Mode.MINIMIZE,
-                             c=[-2, 2, -6, 0, 0, 0],
-                             a=[
-                                 [2, 1, -2, 1, 0, 0],
-                                 [1, 2, 4, 0, 1, 0],
-                                 [1, -1, 2, 0, 0, 1]
-                             ],
-                             b=[24, 23, 10],
-                             start=[1, 1, 1, 23, 16, 8],
-                             alpha=0.9,
-                             eps=5)
-
-solver.solve()
-
-
-print("Simplex")
-solver = SimplexSolver(mode=InteriorPointSolver.Mode.MINIMIZE,
-                       c=[-2, 2, -6],
-                       a=[
-                           [2, 1, -2],
-                           [1, 2, 4],
-                           [1, -1, 2]
-                       ],
-                       b=[24, 23, 10],
-                       eps=5)
-
-sol,x1 =solver.solve()
